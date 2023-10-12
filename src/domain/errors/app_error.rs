f333ee44c19074c74bef4b7614c69b837a927b63
Walaff Fernandes <lncitador@icloud.com>
2023-10-12 10:34:50 -0300

    feat(domain/errors): adiciona módulo de erros (master)
    - Error de Dominio
    - Error de Aplicacao

---
+++ src/domain/errors/app_error.rs
@@ -0,0 +1,31 @@
+use std::error::Error;
+use std::fmt;
+use std::fmt::{Debug, Display, Formatter};
+
+pub struct AppError {
+    message: String,
+    code: u32,
+}
+
+impl AppError {
+    pub fn new(message: &str, code: u32) -> AppError {
+        AppError {
+            message: message.to_string(),
+            code,
+        }
+    }
+}
+
+impl Error for AppError {}
+
+impl Display for AppError {
+    fn fmt(&self, f: &mut Formatter) -> fmt::Result {
+        write!(f, "Application Error: {}, code: {}", self.message, self.code)
+    }
+}
+
+impl Debug for AppError {
+    fn fmt(&self, f: &mut Formatter) -> fmt::Result {
+        write!(f, "Application Error: {}, code: {}", self.message, self.code)
+    }
+}
---
+++ src/domain/errors/domain_error.rs
@@ -0,0 +1,37 @@
+use std::error::Error;
+use std::fmt;
+use std::fmt::{Debug, Display, Formatter};
+
+pub struct DomainError {
+    message: String,
+    description: Option<String>
+}
+
+impl DomainError {
+    pub fn new(message: &str, description: &str) -> DomainError {
+        DomainError {
+            message: message.to_string(),
+            description: if description.is_empty() { None } else { Some(description.to_string()) }
+        }
+    }
+}
+
+impl Error for DomainError {}
+
+impl Display for DomainError {
+    fn fmt(&self, f: &mut Formatter) -> fmt::Result {
+        match self.description {
+            Some(ref description) => write!(f, "Domain Error: {}\nDescription: {}", self.message, description),
+            None => write!(f, "Domain Error: {}", self.message)
+        }
+    }
+}
+
+impl Debug for DomainError {
+    fn fmt(&self, f: &mut Formatter) -> fmt::Result {
+        match self.description {
+            Some(ref description) => write!(f, "Domain Error: {}\nDescription: {}", self.message, description),
+            None => write!(f, "Domain Error: {}", self.message)
+        }
+    }
+}
