c456996194c4585b73abc92d68e02aca1a6da49d
Walaff Fernandes <lncitador@icloud.com>
2023-10-12 22:10:56 -0300

    feat(value_objects): adiciona validação de URL (master)
    - Adiciona expressão regular para validar URLs
    - Verifica se a URL contém um domínio comercial
    - Retorna erro se a URL for inválida ou não tiver um domínio comercial

--- src/domain/value_objects/url.rs
+++ src/domain/value_objects/url.rs
@@ -1,7 +1,13 @@
-use std::str::FromStr;
-use http::Uri;
+use lazy_static::lazy_static;
 use crate::domain::errors::domain_error::DomainError;
 use crate::domain::value_objects::{ValueObject, ValueObjectTrait};
+use regex::Regex;
+
+lazy_static! {
+    pub static ref URL_REGEX: Regex = Regex::new(
+        r"https?://(?:www\.)?([-a-zA-Z0-9@:%._\+~#=]{2,256}\.[a-z]{2,6}\b)*(/[/\d\w\.-]*)*(?:[\?])*(.+)*"
+    ).unwrap();
+}
 
 pub type UrlEntity = ValueObject<String>;
 
@@ -23,9 +29,23 @@ impl ValueObjectTrait<String> for UrlEntity {
     fn new(value: Option<&str>) -> Result<UrlEntity, DomainError> {
         match value {
             Some(value) => {
-                match Uri::from_str(value) {
-                    Ok(value) => Ok(ValueObject { value: value.to_string() }),
-                    Err(_) => Err(DomainError::new("Invalid URL", ""))
+                if URL_REGEX.is_match(value) {
+                    let groups = URL_REGEX.captures(value).unwrap();
+                    let path = groups.get(1);
+
+                    if let Some(path) = path {
+                        if !path.as_str().contains(".com") {
+                            return Err(DomainError::new("Can't commercial URL", ""))
+                        }
+                    } else {
+                        return Err(DomainError::new("Invalid URL", ""))
+                    }
+
+                    Ok(UrlEntity {
+                        value: value.to_string()
+                    })
+                } else {
+                    Err(DomainError::new("Invalid URL", ""))
                 }
             },
             None => Err(DomainError::new("URL is required", ""))
