fc80b768a15a322ca1530c05be258b13847d4a6b
Walaff Fernandes <lncitador@icloud.com>
2023-10-12 23:44:19 -0300

    feat(errors): Adiciona função as_description no arquivo domain_error.rs (master)

--- src/domain/errors/domain_error.rs
+++ src/domain/errors/domain_error.rs
@@ -35,3 +35,34 @@ impl Debug for DomainError {
         }
     }
 }
+
+pub fn as_description(errors: Vec<DomainError>) -> String {
+    let mut description = String::from("");
+
+    for (i, error) in errors.iter().enumerate() {
+        description.push_str(error.message.as_str());
+
+        for (i, error) in errors.iter().enumerate() {
+            if i == 0 {
+                if errors.len() == 1 {
+                    description = error.to_string();
+                } else {
+                    description.push_str("[\n");
+                    description.push_str("  ");
+                    description.push_str(error.to_string().as_str());
+                    description.push_str(",\n");
+                }
+            } else if i == errors.len() - 1 {
+                description.push_str("  ");
+                description.push_str(error.to_string().as_str());
+                description.push_str("\n]");
+            } else {
+                description.push_str("  ");
+                description.push_str(error.to_string().as_str());
+                description.push_str(",\n");
+            }
+        }
+    }
+
+    description
+}
