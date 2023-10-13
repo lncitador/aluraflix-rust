249fc217587e667b6f94364f43c67b18b76c6817
Walaff Fernandes <lncitador@icloud.com>
2023-10-13 00:24:53 -0300

    feat(main): adiciona configuração de variável de ambiente do banco de dados

--- src/main.rs
+++ src/main.rs
@@ -1,12 +1,16 @@
 use std::env;
+use crate::domain::entities::categories::{Categories, CategoriesInput};
+use crate::domain::entities::videos::{Videos, VideosInput};
 use crate::domain::value_objects::unique_id::UniqueEntityID;
 use crate::domain::value_objects::ValueObjectTrait;
 
 mod domain;
 
 fn main() {
-    let database_url = env::var("DATABASE_URL").expect("DATABASE_URL must be set");
-    let id = UniqueEntityID::new("");
+    let database_url = env::var("DATABASE_URL")
+        .unwrap_or(String::from("postgres://postgres:postgres@db:6011/aluraflix-dev"));
+
+    let id = UniqueEntityID::new(Some("018b2444-d876-751b-b1f0-838ecea39578"));
 
     match id {
         Ok(id) => println!("{}", id.to_string()),
@@ -14,4 +18,38 @@ fn main() {
     }
 
     println!("{}", database_url);
+
+    let category_input = &CategoriesInput{
+        name: String::from("Teste"),
+        color: String::from("rgb(255, 255, 255)"),
+        user_id: String::from("018b2444-d876-751b-b1f0-838ecea39578")
+    };
+
+    let category = match Categories::new(category_input) {
+        Ok(category) => {
+            println!("{:#?}", category);
+
+            category
+        },
+        Err(err) => {
+            println!("{}", err.to_string());
+
+            return;
+        }
+    };
+
+    let input = &VideosInput{
+        title: String::from("Teste"),
+        description: String::from("Teste"),
+        url: String::from("https://www.youtube.com"),
+        user_id: String::from("018b2444-d876-751b-b1f0-838ecea39578"),
+        category_id: category.id.to_string()
+    };
+
+    let video = Videos::new( input);
+
+    match video {
+        Ok(video) => println!("{:#?}", video),
+        Err(err) => println!("{}", err.to_string())
+    }
 }
