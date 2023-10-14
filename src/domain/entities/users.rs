15fd6d2fee7376cd2ccdfdc3b85d297deaa050af
Walaff Fernandes <lncitador@icloud.com>
2023-10-13 23:14:03 -0300

    feat(users): adiciona entidade Users (master)
    Adiciona a entidade Users no arquivo users.rs do diretório src/domain/entities. A entidade possui os seguintes campos:
    - id: UniqueEntityID
    - name: String
    - email: EmailEntity
    - password: String
    - created_at: Date
    - updated_at: Date
    
    Implementa a função new() para criar uma nova instância da entidade com base nos dados fornecidos. A função realiza as seguintes verificações:
    - Verifica se o campo name possui no mínimo 4 caracteres. Caso contrário, retorna um erro.
    - Verifica se o campo email é válido. Caso contrário, retorna um erro.
    - Verifica se o campo password possui no mínimo 8 caracteres. Caso contrário, retorna um erro.
    
    Se houver erros, retorna um erro com a mensagem "Invalid data" e a descrição dos erros encontrados.

---
+++ src/domain/entities/users.rs
@@ -0,0 +1,80 @@
+use serde::{Deserialize, Serialize};
+use time::{Date, OffsetDateTime};
+use crate::domain::errors::domain_error::{as_descriptions, DomainError};
+use crate::domain::value_objects::email::EmailEntity;
+use crate::domain::value_objects::unique_id::UniqueEntityID;
+use crate::domain::value_objects::ValueObjectTrait;
+
+#[derive(Deserialize)]
+pub struct UsersInput {
+    pub name: String,
+    pub email: String,
+    pub password: String,
+}
+
+#[derive(Debug, Serialize, Deserialize)]
+pub struct Users {
+    pub id: UniqueEntityID,
+    pub name: String,
+    pub email: EmailEntity,
+    pub password: String,
+    pub created_at: Date,
+    pub updated_at: Date,
+}
+
+impl Users {
+    pub fn new(data: &UsersInput) -> Result<Self, DomainError> {
+        let id = UniqueEntityID::new(None).unwrap();
+
+        let now = OffsetDateTime::now_utc().date();
+
+        let mut errors: Vec<DomainError> = vec![];
+
+        let name = match data.name.len() {
+            0 => {
+                errors.push(DomainError::new("Name is required", ""));
+                None
+            },
+            1 | 2 | 3 => {
+                errors.push(DomainError::new("Name must be at least 4 characters", ""));
+                None
+            },
+            _ => Some(data.name.to_string())
+        };
+
+        let email = match EmailEntity::new(Some(data.email.as_str())) {
+            Ok(email) => Some(email),
+            Err(error) => {
+                errors.push(error);
+                None
+            }
+        };
+
+        let password = match data.password.len() {
+            0 => {
+                errors.push(DomainError::new("Password is required", ""));
+                None
+            },
+            1 | 2 | 3 | 4 | 5 | 6 | 7  => {
+                errors.push(DomainError::new("Password must be at least 8 characters", ""));
+                None
+            },
+            _ => Some(data.password.to_string())
+        };
+
+        if errors.len() > 0 {
+            let description = as_descriptions(errors);
+
+            return Err(DomainError::new("Invalid data", description.as_str()))
+        }
+
+        Ok(Users {
+            id,
+            name: name.unwrap(),
+            email: email.unwrap(),
+            password: password.unwrap(),
+            created_at: now,
+            updated_at: now,
+        })
+    }
+}
