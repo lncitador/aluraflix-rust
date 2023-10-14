ff5739328091da7393ae4b1bd50600023af60022
Walaff Fernandes <lncitador@icloud.com>
2023-10-14 00:36:59 -0300

    feat(repositories): adiciona módulos para vídeos, categorias, usuários e memória (master)

--- src/application/repositories/mod.rs
+++ src/application/repositories/mod.rs
@@ -1,5 +1,11 @@
 use crate::domain::value_objects::unique_id::UniqueEntityID;
 
+pub mod videos;
+pub mod categories;
+pub mod users;
+pub mod in_memory;
+
+
 pub trait Repository<T> {
     fn find_all(&self) -> Vec<T>;
     fn find_by_id(&self, id: UniqueEntityID) -> Option<T>;
