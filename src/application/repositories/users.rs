f0ce3fb26d5c21c92bf670c387df6d21fbb7cd5d
Walaff Fernandes <lncitador@icloud.com>
2023-10-14 01:55:39 -0300

    feat(users): atualiza assinatura do método find_by_email (master)
    Atualiza a assinatura do método find_by_email no repositório de usuários para receber um objeto EmailEntity em vez de uma string.

--- src/application/repositories/users.rs
+++ src/application/repositories/users.rs
@@ -1,6 +1,7 @@
 use crate::application::repositories::Repository;
 use crate::domain::entities::users::Users;
+use crate::domain::value_objects::email::EmailEntity;
 
 pub trait UsersRepository: Repository<Users> {
-    fn find_by_email(&self, email: String) -> Option<Users>;
+    fn find_by_email(&self, email: EmailEntity) -> Option<Users>;
 }
