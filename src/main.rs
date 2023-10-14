3710affd3c49b6e4464e9db94fcbf0494780e86d
Walaff Fernandes <lncitador@icloud.com>
2023-10-13 23:15:18 -0300

    feat: adicionar criação de usuário ao arquivo main.rs (main)
    Adiciona a criação de usuário ao arquivo main.rs, permitindo que novos usuários sejam cadastrados no sistema. Utiliza a estrutura UsersInput para receber os dados do usuário e a estrutura Users para realizar a validação e criação do usuário. Ao final, o usuário é impresso em caso de sucesso. Caso ocorra algum erro, a mensagem de erro é exibida.

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,6 @@
 use std::env;
 use crate::domain::entities::categories::{Categories, CategoriesInput};
+use crate::domain::entities::users::{Users, UsersInput};
 use crate::domain::entities::videos::{Videos, VideosInput};
 use crate::domain::value_objects::unique_id::UniqueEntityID;
 use crate::domain::value_objects::ValueObjectTrait;
@@ -17,12 +18,32 @@ fn main() {
         Err(err) => println!("{}", err.to_string())
     }
 
+    let user_input = &UsersInput{
+        name: String::from("Teste"),
+        email: String::from("johndoe@test.com"),
+        password: String::from("12345678")
+    };
+
+    let user = match Users::new(user_input) {
+        Ok(user) => {
+            println!("{:#?}", user);
+
+            user
+        },
+        Err(err) => {
+            println!("{}", err.to_string());
+
+            return;
+        }
+    };
+
+
     println!("{}", database_url);
 
     let category_input = &CategoriesInput{
         name: String::from("Teste"),
         color: String::from("rgb(255, 255, 255)"),
-        user_id: String::from("018b2444-d876-751b-b1f0-838ecea39578")
+        user_id: user.id.to_string(),
     };
 
     let category = match Categories::new(category_input) {
@@ -42,7 +63,7 @@ fn main() {
         title: String::from("Teste"),
         description: String::from("Teste"),
         url: String::from("https://www.youtube.com"),
-        user_id: String::from("018b2444-d876-751b-b1f0-838ecea39578"),
+        user_id: user.id.to_string(),
         category_id: category.id.to_string()
     };
 
