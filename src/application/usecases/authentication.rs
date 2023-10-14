ec7641b79b361e8d2e4b954a5ce3fd8f2fc6ba0a
Walaff Fernandes <lncitador@icloud.com>
2023-10-14 18:31:35 -0300

    feat(authentication): implementar caso de uso de login e cadastro de usuários
    Implementa os métodos `sign_in` e `sign_up` no caso de uso de autenticação.
    
    O método `sign_in` recebe um objeto `SignInInput` contendo o email e senha do usuário e retorna uma string "User logged in" se o login for bem-sucedido. Caso contrário, retorna um erro `AuthUseCaseError`, com uma das seguintes mensagens de erro:
    
    - "User not found" se o email não estiver cadastrado no sistema.
    - "Invalid password" se a senha informada não coincidir com a senha do usuário.
    
    O método `sign_up` recebe um objeto `UsersInput` contendo os dados do usuário a ser cadastrado e retorna um objeto `Users` se o cadastro for bem-sucedido. Caso contrário, retorna um erro `AuthUseCaseError`, com uma das seguint

--- src/application/usecases/authentication.rs
+++ src/application/usecases/authentication.rs
@@ -1,11 +1,14 @@
-use std::rc::Rc;
-use crate::application::repositories::users::UsersRepository;
+use std::fmt::{Debug, Formatter};
+use crate::application::repositories::RepositoryError;
+use crate::application::repositories::users::{UsersRepository, UsersRepositoryContract};
+use crate::domain::entities::users::{Users, UsersInput};
 use crate::domain::errors::app_error::AppError;
+use crate::domain::errors::domain_error::DomainError;
 use crate::domain::value_objects::email::EmailEntity;
 use crate::domain::value_objects::ValueObjectTrait;
 
 pub struct AuthUseCase {
-    pub users_repository: Rc<dyn UsersRepository>,
+    pub users_repository: UsersRepositoryContract
 }
 
 pub struct SignInInput {
@@ -14,24 +17,83 @@ pub struct SignInInput {
 }
 
 impl AuthUseCase {
-    pub fn new(users_repository: Rc<dyn UsersRepository>) -> Self {
+    pub fn new(users_repository: UsersRepositoryContract) -> Self {
         Self {
             users_repository,
         }
     }
 
-    pub fn sign_in(&self, input: SignInInput) -> Result<String, AppError> {
+    pub fn sign_in(&self, input: SignInInput) -> Result<String, AuthUseCaseError> {
         let email = match EmailEntity::new(Some(&input.email)) {
             Ok(email) => email,
-            Err(error) => return Err(AppError::new(&error.to_string(), 400)),
+            Err(_) => return Err(AuthUseCaseError::UserNotFound),
         };
 
-        let user = self.users_repository.find_by_email(email).ok_or(AppError::new("User not found", 404))?;
+        let mut users_repository = self.users_repository.clone();
+
+        let user = match users_repository.lock() {
+            Ok(repo) => {
+                match repo.find_by_email(email) {
+                    Some(user) => user,
+                    None => return Err(AuthUseCaseError::UserNotFound),
+                }
+            }
+            Err(err) => {
+                return Err(AuthUseCaseError::Domain(DomainError::new("Repository lock error", err.to_string().as_str())));
+            }
+        };
+
+        // if !user.password.is_valid(&input.password) {
+        //    return Err(AuthUseCaseError::InvalidPassword);
+        // }
 
         if user.password != input.password {
-            return Err(AppError::new("Invalid password", 401));
+            return Err(AuthUseCaseError::InvalidPassword);
         }
 
         Ok("User logged in".to_string())
+    }
+
+    pub fn sign_up(&mut self, input: UsersInput) -> Result<Users, AuthUseCaseError> {
+        let email = match EmailEntity::new(Some(&input.email)) {
+            Ok(email) => email,
+            Err(error) => return Err(AuthUseCaseError::Domain(error)),
+        };
+
+
+        let user_already_exists = match self.users_repository.lock() {
+            Ok(repo) => {
+                match repo.find_by_email(email) {
+                    Some(_) => true,
+                    None => false,
+                }
+            }
+            Err(err) => {
+                return Err(AuthUseCaseError::Domain(DomainError::new("Repository lock error", err.to_string().as_str())));
+            }
+        };
+
+        if user_already_exists {
+            return Err(AuthUseCaseError::UserAlreadyExists);
+        }
+
+        let user = match Users::new(&input) {
+            Ok(user) => user,
+            Err(error) => {
+                return Err(AuthUseCaseError::Domain(error));
+            }
+        };
+
+        match self.users_repository.lock() {
+            Ok(mut repo) => {
+                match repo.save(user) {
+                    Ok(user) => Ok(user),
+                    Err(error) => Err(AuthUseCaseError::from(error)),
+                }
+            }
+            Err(err) => {
+                Err(AuthUseCaseError::Domain(DomainError::new("Repository lock error", err.to_string().as_str())))
+            }
+        }
     }
 }
