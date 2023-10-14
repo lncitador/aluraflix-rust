943c654ccee1db1f2d4e358fc701fbb88a06c72e
Walaff Fernandes <lncitador@icloud.com>
2023-10-14 01:55:27 -0300

    feat(repositories): altera assinatura do método delete para receber um &mut self (main)

--- src/application/repositories/mod.rs
+++ src/application/repositories/mod.rs
@@ -3,12 +3,11 @@ use crate::domain::value_objects::unique_id::UniqueEntityID;
 pub mod videos;
 pub mod categories;
 pub mod users;
-pub mod in_memory;
 
 
 pub trait Repository<T> {
     fn find_all(&self) -> Vec<T>;
     fn find_by_id(&self, id: UniqueEntityID) -> Option<T>;
     fn save(&mut self, entity: T);
-    fn delete(&self, id: UniqueEntityID) -> bool;
+    fn delete(&mut self, id: UniqueEntityID) -> bool;
 }
