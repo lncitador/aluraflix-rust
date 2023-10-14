0e4fa6ec7b4337e204cc6d9ec9b861680a7b86de
Walaff Fernandes <lncitador@icloud.com>
2023-10-14 00:32:50 -0300

    feat(repo): adiciona repositório in-memory de vídeos (master)
    Adiciona o arquivo `videos.rs` ao diretório `src/application/repositories/in_memory`. O arquivo implementa o repositório `VideosRepositoryInMemory`, responsável por armazenar os vídeos em memória. O repositório possui métodos para buscar todos os vídeos, buscar vídeos por ID, salvar um novo vídeo e deletar um vídeo existente. Além disso, implementa o método `find_by_category_id`, que retorna todos os vídeos de uma determinada categoria.
    
    Essa funcionalidade foi adicionada ao repositório do projeto Aluraflix-rust.

---
+++ src/application/repositories/in_memory/videos.rs
@@ -0,0 +1,48 @@
+use crate::application::repositories::Repository;
+use crate::application::repositories::videos::VideosRepository;
+use crate::domain::entities::videos::{Videos};
+use crate::domain::value_objects::unique_id::UniqueEntityID;
+
+pub struct VideosRepositoryInMemory {
+    pub videos: Vec<Videos>,
+}
+
+impl VideosRepositoryInMemory {
+    pub fn new() -> Self {
+        Self { videos: vec![] }
+    }
+
+    fn len(&self) -> usize {
+        self.videos.len()
+    }
+}
+
+impl Repository<Videos> for VideosRepositoryInMemory {
+    fn find_all(&self) -> Vec<Videos> {
+        if self.len() > 10 {
+            panic!("Too many videos in memory, please clear the memory before running the tests");
+        }
+
+        self.videos.clone()
+    }
+
+    fn find_by_id(&self, id: UniqueEntityID) -> Option<Videos> {
+        self.videos.iter().find(|v| v.id == id).cloned()
+    }
+
+    fn save(&mut self, entity: Videos) {
+        self.videos.push(entity);
+    }
+
+    fn delete(&mut self, id: UniqueEntityID) -> bool {
+        let len = self.len();
+        self.videos.retain(|v| v.id != id);
+        len != self.len()
+    }
+}
+
+impl VideosRepository for VideosRepositoryInMemory {
+    fn find_by_category_id(&self, category_id: UniqueEntityID) -> Vec<Videos> {
+        self.videos.iter().filter(|v| v.category_id == category_id).cloned().collect()
+    }
+}
