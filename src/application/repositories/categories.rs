ae6e7908b1db038738e903d47b230da86c582fc2
Walaff Fernandes <lncitador@icloud.com>
2023-10-15 10:08:01 -0300

    feat(categories): adiciona async trait para buscar categorias por ID de categoria e ID de usuário

--- src/application/repositories/categories.rs
+++ src/application/repositories/categories.rs
@@ -1,8 +1,10 @@
+use async_trait::async_trait;
 use crate::application::repositories::Repository;
 use crate::domain::entities::categories::Categories;
 use crate::domain::value_objects::unique_id::UniqueEntityID;
 
+#[async_trait]
 pub trait CategoriesRepository: Repository<Categories> {
-    fn find_by_category_id(&self, category_id: UniqueEntityID) -> Vec<Categories>;
-    fn find_by_user_id(&self, user_id: UniqueEntityID) -> Vec<Categories>;
+    async fn find_by_category_id(&self, category_id: UniqueEntityID) -> Vec<Categories>;
+    async fn find_by_user_id(&self, user_id: UniqueEntityID) -> Vec<Categories>;
 }
