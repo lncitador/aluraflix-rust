3b0d479e0cc1c1992757fb111b66e36a21c6d8e9
Walaff Fernandes <lncitador@icloud.com>
2023-10-15 10:07:02 -0300

    feat(repositories): adiciona trait VideosRepository com método find_by_category_id (master)
    Adiciona a trait VideosRepository com o método find_by_category_id, que retorna uma
    lista de vídeos com base no ID da categoria. Implementa a trait async_trait para
    habilitar a utilização de métodos assíncronos.

--- src/application/repositories/videos.rs
+++ src/application/repositories/videos.rs
@@ -1,7 +1,9 @@
+use async_trait::async_trait;
 use crate::application::repositories::Repository;
 use crate::domain::entities::videos::Videos;
 use crate::domain::value_objects::unique_id::UniqueEntityID;
 
+#[async_trait]
 pub trait VideosRepository: Repository<Videos> {
-    fn find_by_category_id(&self, category_id: UniqueEntityID) -> Vec<Videos>;
+   async fn find_by_category_id(&self, category_id: UniqueEntityID) -> Vec<Videos>;
 }
