e97ce25c4ba246bac9828b25fa24685ace708b1d
Walaff Fernandes <lncitador@icloud.com>
2023-10-15 10:06:49 -0300

    feat(users): adiciona trait UsersRepository e método find_by_email
    Adiciona a trait UsersRepository que representa o repositório de usuários
    e o método find_by_email para buscar um usuário pelo email.

--- src/application/repositories/users.rs
+++ src/application/repositories/users.rs
@@ -1,10 +1,12 @@
 use std::sync::{Arc, Mutex};
+use async_trait::async_trait;
 use crate::application::repositories::Repository;
 use crate::domain::entities::users::Users;
 use crate::domain::value_objects::email::EmailEntity;
 
+#[async_trait]
 pub trait UsersRepository: Repository<Users> {
-    fn find_by_email(&self, email: EmailEntity) -> Option<Users>;
+    async fn find_by_email(&self, email: EmailEntity) -> Option<Users>;
 }
 
 pub type UsersRepositoryContract = Arc<Mutex<dyn UsersRepository>>;
