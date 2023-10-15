d5a2ab34da150297355e557d5528f3009d127a53
Walaff Fernandes <lncitador@icloud.com>
2023-10-15 10:51:00 -0300

    feat(categories): Adiciona implementação de repositório de categorias
    - Adiciona importação de std::sync::{Arc, Mutex}
    - Implementa trait CategoriesRepository
    - Adiciona métodos find_by_category_id() e find_by_user_id()
    - Adiciona tipo CategoriesRepositoryContract como Arc<Mutex<dyn CategoriesRepository>>

--- src/application/repositories/categories.rs
+++ src/application/repositories/categories.rs
@@ -1,3 +1,4 @@
+use std::sync::{Arc, Mutex};
 use async_trait::async_trait;
 use crate::application::repositories::Repository;
 use crate::domain::entities::categories::Categories;
@@ -8,3 +9,5 @@ pub trait CategoriesRepository: Repository<Categories> {
     async fn find_by_category_id(&self, category_id: UniqueEntityID) -> Vec<Categories>;
     async fn find_by_user_id(&self, user_id: UniqueEntityID) -> Vec<Categories>;
 }
+
+pub type CategoriesRepositoryContract = Arc<Mutex<dyn CategoriesRepository>>;
